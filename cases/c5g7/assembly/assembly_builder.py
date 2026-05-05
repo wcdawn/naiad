f0082ffe32a293a234479a5cca1bb4059ecb9033
William Dawn <wdawn730@gmail.com>
2026-05-05 13:02:25 -0600

    writing input generator

---
+++ cases/c5g7/assembly/assembly_builder.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+
+    mat_uo2 = ["moderator", "uo2", "moderator"]
+    dx_uo2 = [0.09, 1.08, 0.09]
+
+    mat_mox43 = ["moderator", "mox43", "moderator"]
+    dx_mox43 = [0.09, 1.08, 0.09]
+
+    mat_mox70 = ["moderator", "mox70", "moderator"]
+    dx_mox70 = [0.09, 1.08, 0.09]
+
+    mat_mox87 = ["moderator", "mox87", "moderator"]
+    dx_mox87 = [0.09, 1.08, 0.09]
