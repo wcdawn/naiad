74a7fd4eb4167eed531f3d80275b48a1e1b89971
William Dawn <wdawn730@gmail.com>
2026-05-05 15:39:58 -0600

    more work on assembly construction

--- cases/c5g7/assembly/assembly_builder.py
+++ cases/c5g7/assembly/assembly_builder.py
@@ -1,3 +1,5 @@
+import sys
+
 if __name__ == "__main__":
 
     mat_uo2 = ["moderator", "uo2", "moderator"]
@@ -11,3 +13,76 @@ if __name__ == "__main__":
 
     mat_mox87 = ["moderator", "mox87", "moderator"]
     dx_mox87 = [0.09, 1.08, 0.09]
+
+    mat_gdt = ["moderator", "guide_tube", "moderator"]
+    dx_gdt = [0.09, 1.08, 0.09]
+
+    assembly_uo2 = [
+        "uo2",
+        "uo2",
+        "gdt",
+        "uo2",
+        "uo2",
+        "gdt",
+        "uo2",
+        "uo2",
+        "gdt",
+        "uo2",
+        "uo2",
+        "gdt",
+        "uo2",
+        "uo2",
+        "gdt",
+        "uo2",
+        "uo2",
+    ]
+
+    assembly_mox = [
+        "mox43",
+        "mox70",
+        "gdt",
+        "mox87",
+        "mox87",
+        "gdt",
+        "mox87",
+        "mox87",
+        "gdt",
+        "mox87",
+        "mox87",
+        "gdt",
+        "mox87",
+        "mox87",
+        "gdt",
+        "mox70",
+        "mox43",
+    ]
+
+    dx = []
+    mat = []
+    for pin in assembly_mox:
+        if pin == "uo2":
+            dx += dx_uo2
+            mat += mat_uo2
+        elif pin == "mox43":
+            dx += dx_mox43
+            mat += mat_mox43
+        elif pin == "mox70":
+            dx += dx_mox70
+            mat += mat_mox70
+        elif pin == "mox87":
+            dx += dx_mox87
+            mat += mat_mox87
+        elif pin == "gdt":
+            dx += dx_gdt
+            mat += mat_gdt
+        else:
+            print("unknown pin: ", pin)
+            sys.exit(1)
+
+    print("nx ", len(dx))
+    print("dx")
+    for x in dx:
+        print("{:.2f}".format(x))
+    print("mat_map")
+    for m in mat:
+        print(m)
