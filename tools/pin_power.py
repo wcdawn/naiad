b12cbc3dcbc83e8d2ece276043abf9b332cd3f82
William Dawn <wdawn730@gmail.com>
2026-05-20 15:47:09 -0600

    formatting

--- tools/pin_power.py
+++ tools/pin_power.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 
+
 def compute_dx(x):
     dx = np.zeros_like(x)
     xleft = 0.0
@@ -26,13 +27,13 @@ if __name__ == "__main__":
     xmax = x[-1] + 0.5 * dx[-1]
     npin = 0
     while pin_edges[-1] < xmax:
-        pin_edges.append(pin_pitch*(npin+1))
+        pin_edges.append(pin_pitch * (npin + 1))
         npin += 1
 
     pin_power = np.zeros(npin)
     for i in range(npin):
-        f1 = (x > pin_edges[i])
-        f2 = (x < pin_edges[i+1])
+        f1 = x > pin_edges[i]
+        f2 = x < pin_edges[i + 1]
         filt = f1 & f2
         pin_power[i] = np.sum(power[filt] * dx[filt])
 
@@ -43,7 +44,7 @@ if __name__ == "__main__":
 
     count = 0
     for i in range(npin):
-        if (pin_power[i] > 0.0):
+        if pin_power[i] > 0.0:
             count += 1
-            #print("{:d} {:.3f}".format(count, pin_power[i]))
+            # print("{:d} {:.3f}".format(count, pin_power[i]))
             print("{:.3f}".format(pin_power[i]))
